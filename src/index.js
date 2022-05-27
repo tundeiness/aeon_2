3abc0ce34b37d3afc2beb3f9f33ebc6ca3273f2c
tundeiness <tundeiness@yahoo.co.uk>
2022-05-27 12:50:27 +0100

    add context provider

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import ContextProvider from './contexts/ContextProvider';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter forceRefresh>
     <React.StrictMode>
-      <App />
+      <ContextProvider>
+        <App />
+      </ContextProvider>
     </React.StrictMode>
   </BrowserRouter>,
 );
