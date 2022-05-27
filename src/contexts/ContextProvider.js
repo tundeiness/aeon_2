d9cddbcf9f58bff42ce3729e63124445e34194f2
tundeiness <tundeiness@yahoo.co.uk>
2022-05-27 11:30:25 +0100

    create context

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,28 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React, { createContext, useContext, useState } from 'react';
+
+const ContextProvider = ({ children }) => {
+  const test = 0;
+  const initialState = {
+    serverStatus: false,
+  };
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
+  const StateContext = createContext();
+
+  const handleClick = (clicked) => {
+    setIsClicked({ ...initialState, [clicked]: true });
+  };
+
+  return (
+    <StateContext value={{
+      activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick,
+    }}
+    >
+      {children}
+    </StateContext>
+  );
+};
+
+export default ContextProvider;
