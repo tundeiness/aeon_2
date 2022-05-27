c08dd296a1c2550e85863674a9ccce26e2f9831d
tundeiness <tundeiness@yahoo.co.uk>
2022-05-27 09:45:55 +0100

    add prop-types as dependence

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Suspense } from 'react';
 import { useRoutes } from 'react-router-dom';
 import PageLoader from './components/pageLoader/pageLoader';
 
-import routes from '../Routes';
+import routes from './Routes';
 import './App.css';
 
 const App = () => {
