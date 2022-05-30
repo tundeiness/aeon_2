624ff978bd65fbfcd8bd1c3d4b49f5882d8eadc0
tundeiness <tundeiness@yahoo.co.uk>
2022-05-30 18:38:47 +0100

    edit institution

---
+++ src/pages/layout/institutions/createInstituition/createInstituition.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const CreateInstituition = () => {
+  const test = 0;
+  return (
+    <div>createInstituition</div>
+  );
+};
+
+export default CreateInstituition;
