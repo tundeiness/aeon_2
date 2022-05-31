a5b2a769035c94dd5f0aae58a618a09aacfc53ad
tundeiness <tundeiness@yahoo.co.uk>
2022-05-31 10:59:34 +0100

    edit instituition

--- src/pages/layout/institutions/institutions.jsx
+++ src/pages/layout/institutions/institutions.jsx
@@ -12,6 +12,8 @@ import { Routes, Link, Outlet } from 'react-router-dom';
 import ReactPaginate from 'react-paginate';
 import SupportButton from '../../../components/support/support';
 import { useStateContext } from '../../../contexts/ContextProvider';
+import SidebarNav from '../../../components/sideBarNav/sidebar-nav';
+import DeleteInstitution from './deleteInstitution/DeleteInstitution';
 import Data from '../../../data/MOCK_DATA.json';
 
 const Institutions = () => {
@@ -76,79 +78,74 @@ const Institutions = () => {
     setPageNum(selected);
   };
   return (
-  // <article className="flex-1">
+    // <article className="flex-1">
+    <div className="relative flex min-h-screen overflow-hidden">
+      <SidebarNav />
+      <article className="w-4/5 ml-auto">
+        <section className="pt-3 pl-4 h-full bg-liteBlue pb-5">
+          <div className="institution-wrapper p-5 bg-white rounded-tl-3xl rounded-bl-3xl">
+            <header className="flex justify-between mb-2">
+              <h1 className="institution-header mb-3 font-medium text-3xl">
+                Institutions
+                {' '}
+              </h1>
+              <SupportButton />
+            </header>
 
-    <article className="w-4/5 ml-auto">
-      <section className="pt-3 pl-4 h-full bg-liteBlue pb-5">
-        <div className="institution-wrapper p-5 bg-white rounded-tl-3xl rounded-bl-3xl">
-          <header className="flex justify-between mb-2">
-            {/* This is the root dashboard view */}
-            {/* <h1 className="inline-block font-medium text-3xl leading-9">
-              Dashboard
-            </h1> */}
-            <h1 className="institution-header mb-3 font-medium text-3xl">
-              Institutions
-              {' '}
-            </h1>
-            {/* <div className="flex items-center py-2 rounded-lg bg-supportBg text-gray-900 hover:cursor-pointer">
-              <FiUser className="xl:mx-2" />
-              <p className="xl:mr-2">support@credequity.com</p>
-            </div> */}
-            <SupportButton />
-          </header>
-
-          <hr className="mb-5" />
-          <div className="search-wrapper relative">
-            <FiSearch className="absolute z-10 top-2.5 left-2 text-xl" />
-            <input
-              type="search"
-              placeholder="Search"
-              className="relative py-2 pl-8 w-1/4 rounded-lg outline outline-gray-300"
-            />
-          </div>
-          <div className="name-list">
-            <table className="table-fixed w-full text-xs">
-              <thead className=" bg-gray-50 text-xs capitalize">
-                <tr>
-                  <th
-                    scope="col"
-                    className="w-12 text-gray-500 py-4 px-2 text-left"
-                  >
-                    S/N
-                  </th>
-                  <th
-                    scope="col"
-                    className="w-52 text-gray-500 py-4 text-center"
-                  >
-                    Name
-                  </th>
-                  <th
-                    scope="col"
-                    className=" flex items-center  text-gray-500 py-4 pl-20"
-                  >
-                    Status
-                    {' '}
-                    <BsArrowDownShort />
-                  </th>
-                  <th
-                    scope="col"
-                    className=" text-gray-500 py-4 pl-4 text-left"
-                  >
-                    Website
-                  </th>
-                  <th
-                    scope="col"
-                    className=" text-gray-500  py-4 pl-10 text-left"
-                  >
-                    Teams
-                  </th>
-                  <th scope="col" className=" text-gray-500 py-4 text-left" />
-                </tr>
-              </thead>
-              <tbody className="divide-y divide-gray-300">{displayData}</tbody>
-              {/* <tbody className="w-screen flex justify-start outline outline-black"> */}
-              {' '}
-              {/* <ReactPaginate
+            <hr className="mb-5" />
+            <div className="search-wrapper relative">
+              <FiSearch className="absolute z-10 top-2.5 left-2 text-xl" />
+              <input
+                type="search"
+                placeholder="Search"
+                className="relative py-2 pl-8 w-1/4 rounded-lg outline outline-gray-300"
+              />
+            </div>
+            <div className="name-list">
+              <table className="table-fixed w-full text-xs">
+                <thead className=" bg-gray-50 text-xs capitalize">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="w-12 text-gray-500 py-4 px-2 text-left"
+                    >
+                      S/N
+                    </th>
+                    <th
+                      scope="col"
+                      className="w-52 text-gray-500 py-4 text-center"
+                    >
+                      Name
+                    </th>
+                    <th
+                      scope="col"
+                      className=" flex items-center  text-gray-500 py-4 pl-20"
+                    >
+                      Status
+                      {' '}
+                      <BsArrowDownShort />
+                    </th>
+                    <th
+                      scope="col"
+                      className=" text-gray-500 py-4 pl-4 text-left"
+                    >
+                      Website
+                    </th>
+                    <th
+                      scope="col"
+                      className=" text-gray-500  py-4 pl-10 text-left"
+                    >
+                      Teams
+                    </th>
+                    <th scope="col" className=" text-gray-500 py-4 text-left" />
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-300">
+                  {displayData}
+                </tbody>
+                {/* <tbody className="w-screen flex justify-start outline outline-black"> */}
+                {' '}
+                {/* <ReactPaginate
                 previousLabel="Previous"
                 nextLabel="Next"
                 pageCount={pagingCount}
@@ -160,24 +157,25 @@ const Institutions = () => {
                 activeClassName="paginationActive"
                 className="w-screen flex flex-row justify-around py-3 outline outline-red-500"
               /> */}
-              {/* </tbody> */}
-            </table>
+                {/* </tbody> */}
+              </table>
+            </div>
+            <ReactPaginate
+              previousLabel="Previous"
+              nextLabel="Next"
+              pageCount={pagingCount}
+              onPageChange={changePage}
+              containerClassName="pagination-button"
+              previousLinkClassName="previousButton"
+              nextLinkClassName="nextButton"
+              disabledClassName="paginationDisabled"
+              activeClassName="paginationActive"
+              className="w-full flex flex-row justify-around py-3 text-xs shadow-md"
+            />
           </div>
-          <ReactPaginate
-            previousLabel="Previous"
-            nextLabel="Next"
-            pageCount={pagingCount}
-            onPageChange={changePage}
-            containerClassName="pagination-button"
-            previousLinkClassName="previousButton"
-            nextLinkClassName="nextButton"
-            disabledClassName="paginationDisabled"
-            activeClassName="paginationActive"
-            className="w-full flex flex-row justify-around py-3 text-xs shadow-md"
-          />
-        </div>
-      </section>
-    </article>
+        </section>
+      </article>
+    </div>
   );
 };
 
