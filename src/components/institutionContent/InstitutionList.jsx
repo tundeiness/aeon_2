7efff4212b1a6c8220e9cb0bf2174ad76057f703
tundeiness <tundeiness@yahoo.co.uk>
2022-05-31 11:28:01 +0100

    refactor dummy.js
    create institution list component

---
+++ src/components/institutionContent/InstitutionList.jsx
@@ -0,0 +1,183 @@
+/* eslint-disable max-len */
+/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import ReactPaginate from 'react-paginate';
+import SupportButton from '../support/support';
+import {
+  SearchIcon, ArrowDownIcon, DotIcon, DeleteIcon, CreateIcon,
+} from '../../data/Dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
+import Data from '../../data/MOCK_DATA.json';
+
+export const InstitutionList = () => {
+  const { activeModal, setActiveModal } = useStateContext();
+
+  const [mockData, setMockData] = useState(Data);
+  const [pageNum, setPageNum] = useState(0);
+
+  const dataPerPage = 10;
+  const dataPageVisited = pageNum * dataPerPage;
+
+  const displayData = mockData
+    .slice(dataPageVisited, dataPageVisited + dataPerPage)
+    ?.map((datum) => (
+      <tr key={datum['S/N']}>
+        <td className="text-sm leading-5 py-4 px-3">{datum['S/N']}</td>
+        <td className="py-4 uppercase text-center">{datum.Name}</td>
+        <td className="py-4 pr-4 pl-20">
+          {datum.Status === 'Active' ? (
+            <span className="flex items-center bg-green-300 py-0.3 px-0.2 w-14 rounded-xl text-white">
+              {/* <GoPrimitiveDot className="text-white" /> */}
+              <span className="text-white">{DotIcon}</span>
+              {datum.Status}
+            </span>
+          ) : (
+            <span className="flex items-center bg-red-400 py-0.3 px-0.2 w-16 rounded-xl text-white">
+              {/* <GoPrimitiveDot className="text-white" /> */}
+              <span className="text-white">{DotIcon}</span>
+              {datum.Status}
+            </span>
+          )}
+        </td>
+        <td className="py-4 pl-4">{datum.Website}</td>
+        <td className="py-4 pl-10">
+          <span className="inline-block text-textTeams py-0.5 px-0.4 w-16 bg-indigo-50 rounded-lg text-center hover:cursor-pointer">
+            {datum.Team}
+          </span>
+        </td>
+        <td className="py-4 px-6">
+          <span className="flex justify-between">
+            <button type="button">
+              <span className="inline-block search-icon hover:cursor-pointer w-5 h-5 text-searchColor">
+                {SearchIcon}
+              </span>
+              {/* <FiSearch className="search-icon hover:cursor-pointer w-5 h-5 text-searchColor" /> */}
+            </button>
+            <button
+              type="button"
+               // onClick={() => setActiveModal(true)}
+              onClick={() => setActiveModal((prevActiveModal) => !prevActiveModal)}
+            >
+              {/* <RiDeleteBinLine className="delete-icon hover:cursor-pointer w-5 h-5 text-binColor" /> */}
+              <span className="delete-icon hover:cursor-pointer w-5 h-5 text-binColor">
+                {DeleteIcon}
+              </span>
+            </button>
+            <button type="button">
+              <span className="pen-icon hover:cursor-pointer w-5 h-5 text-penColor">
+                {CreateIcon}
+              </span>
+              {/* <FiEdit2 className="pen-icon hover:cursor-pointer w-5 h-5 text-penColor" /> */}
+            </button>
+          </span>
+        </td>
+      </tr>
+    ));
+
+  // console.log(displayData);
+  const pagingCount = Math.ceil(mockData?.length / dataPerPage);
+
+  const changePage = ({ selected }) => {
+    setPageNum(selected);
+  };
+  return (
+    <article className="w-4/5 ml-auto">
+      <section className="pt-3 pl-4 h-full bg-liteBlue pb-5">
+        <div className="institution-wrapper p-5 bg-white rounded-tl-3xl rounded-bl-3xl">
+          <header className="flex justify-between mb-2">
+            <h1 className="institution-header mb-3 font-medium text-3xl">
+              Institutions
+              {' '}
+            </h1>
+            <SupportButton />
+          </header>
+
+          <hr className="mb-5" />
+          <div className="search-wrapper relative">
+            <span className="inline-block absolute z-10 top-2.5 left-2 text-xl">
+              {/* <FiSearch className="absolute z-10 top-2.5 left-2 text-xl" /> */}
+              {SearchIcon.symbol}
+            </span>
+            <input
+              type="search"
+              placeholder="Search"
+              className="relative py-2 pl-8 w-1/4 rounded-lg outline outline-gray-300"
+            />
+          </div>
+          <div className="name-list">
+            <table className="table-fixed w-full text-xs">
+              <thead className=" bg-gray-50 text-xs capitalize">
+                <tr>
+                  <th
+                    scope="col"
+                    className="w-12 text-gray-500 py-4 px-2 text-left"
+                  >
+                    S/N
+                  </th>
+                  <th
+                    scope="col"
+                    className="w-52 text-gray-500 py-4 text-center"
+                  >
+                    Name
+                  </th>
+                  <th
+                    scope="col"
+                    className=" flex items-center  text-gray-500 py-4 pl-20"
+                  >
+                    Status
+                    <span>{ArrowDownIcon.symbol}</span>
+                    {/* <BsArrowDownShort /> */}
+                  </th>
+                  <th
+                    scope="col"
+                    className=" text-gray-500 py-4 pl-4 text-left"
+                  >
+                    Website
+                  </th>
+                  <th
+                    scope="col"
+                    className=" text-gray-500  py-4 pl-10 text-left"
+                  >
+                    Teams
+                  </th>
+                  <th scope="col" className=" text-gray-500 py-4 text-left" />
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-300">{displayData}</tbody>
+              {/* <tbody className="w-screen flex justify-start outline outline-black"> */}
+              {' '}
+              {/* <ReactPaginate
+                previousLabel="Previous"
+                nextLabel="Next"
+                pageCount={pagingCount}
+                onPageChange={changePage}
+                containerClassName="pagination-button"
+                previousLinkClassName="previousButton"
+                nextLinkClassName="nextButton"
+                disabledClassName="paginationDisabled"
+                activeClassName="paginationActive"
+                className="w-screen flex flex-row justify-around py-3 outline outline-red-500"
+              /> */}
+              {/* </tbody> */}
+            </table>
+          </div>
+          <ReactPaginate
+            previousLabel="Previous"
+            nextLabel="Next"
+            pageCount={pagingCount}
+            onPageChange={changePage}
+            containerClassName="pagination-button"
+            previousLinkClassName="previousButton"
+            nextLinkClassName="nextButton"
+            disabledClassName="paginationDisabled"
+            activeClassName="paginationActive"
+            className="w-full flex flex-row justify-around py-3 text-xs shadow-md"
+          />
+        </div>
+      </section>
+    </article>
+  );
+};
+
+export default InstitutionList;
--- src/data/Dummy.js
+++ src/data/Dummy.js
@@ -2,6 +2,10 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { CgDanger } from 'react-icons/cg';
+import { FiSearch, FiEdit2 } from 'react-icons/fi';
+import { BsArrowDownShort } from 'react-icons/bs';
+import { GoPrimitiveDot } from 'react-icons/go';
+import { RiDeleteBinLine } from 'react-icons/ri';
 
 export const Dummy = () => {
   const test = 0;
@@ -13,4 +17,29 @@ export const Dummy = () => {
 export const DangerIcon = {
   title: 'Danger',
   symbol: <CgDanger />,
+};
+
+export const SearchIcon = {
+  title: 'Search',
+  symbol: <FiSearch />,
+};
+
+export const ArrowDownIcon = {
+  title: 'ArrowDown',
+  symbol: <BsArrowDownShort />,
+};
+
+export const DotIcon = {
+  title: 'Dot',
+  symbol: <GoPrimitiveDot />,
+};
+
+export const DeleteIcon = {
+  title: 'Delete',
+  symbol: <RiDeleteBinLine />,
+};
+
+export const CreateIcon = {
+  title: 'Create',
+  symbol: <FiEdit2 />,
 };
