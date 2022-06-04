16a672ba4aaa39414eccc218792c0e3eceffb98b
tundeiness <tundeiness@yahoo.co.uk>
2022-06-04 12:04:15 +0100

    implement private routes

--- src/utils/PrivateRoute.jsx
+++ src/utils/PrivateRoute.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { Outlet, Navigate } from 'react-router-dom';
 
 const PrivateRoute = () => {
   const data = 0;
+  const auth = { token: false };
   return (
-    <div>PrivateRoute</div>
+    auth.token ? <Outlet /> : <Navigate to="/sign-in" />
   );
 };
 
