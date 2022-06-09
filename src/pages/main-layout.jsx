5861707f9a24103835d74580462db5f715563771
tundeiness <tundeiness@yahoo.co.uk>
2022-06-09 12:39:48 +0100

    create main-layout

---
+++ src/pages/main-layout.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const MainLayout = () => {
+  const test = 0;
+  return (
+    <div>main-layout</div>
+  );
+};
+
+export default MainLayout;
