ae40d17538d8c3aa321d4640fca8b1c5351c546d
tundeiness <tundeiness@yahoo.co.uk>
2022-06-16 11:45:08 +0100

    view institution

--- src/pages/institutions/view-institution/view-institution.jsx
+++ src/pages/institutions/view-institution/view-institution.jsx
@@ -113,25 +113,99 @@ export const ViewInstitution = () => {
                   </dd>
                 </div>
 
-                <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <div className="flex flex-row justify-between bg-gray-50 px-4 py-5">
                   {/* <dt className="text-sm font-medium text-gray-500">
-                    Email address
+                    Full name
                   </dt> */}
-                  <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                    margotfoster@example.com
+                  <dd className="outline outline-red-500 inline-block mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    1,000
+                  </dd>
+                  <dd className="outline outline-red-500 block w-1/3 mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    support@credequity.com
                   </dd>
                 </div>
-                <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+
+                <div className="bg-white flex flex-row justify-between px-4 py-5">
                   {/* <dt className="text-sm font-medium text-gray-500">
-                    Salary expectation
+                    Application for
                   </dt> */}
-                  <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                    $120,000
+                  <dd className="outline outline-red-500 block w-28 mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    Website URL
+                  </dd>
+                  <dd className="outline outline-red-500 block w-1/3 mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    Industry Category
                   </dd>
                 </div>
-                <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <dt className="text-sm font-medium text-gray-500">About</dt>
-                  <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+
+                <div className="flex flex-row justify-between bg-gray-50 px-4 py-5">
+                  {/* <dt className="text-sm font-medium text-gray-500">
+                    Full name
+                  </dt> */}
+                  <dd className="outline outline-red-500 inline-block mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    www.credequity.com
+                  </dd>
+                  <dd className="outline outline-red-500 block w-1/3 mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    Support
+                  </dd>
+                </div>
+
+                <div className="bg-white flex flex-row justify-between px-4 py-5">
+                  {/* <dt className="text-sm font-medium text-gray-500">
+                    Application for
+                  </dt> */}
+                  <dd className="outline outline-red-500 block w-28 mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    Balance
+                  </dd>
+                  <dd className="outline outline-red-500 block w-1/3 mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    Notification Email
+                  </dd>
+                </div>
+
+                <div className="flex flex-row justify-between bg-gray-50 px-4 py-5">
+                  {/* <dt className="text-sm font-medium text-gray-500">
+                    Full name
+                  </dt> */}
+                  <dd className="outline outline-red-500 inline-block mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    0 NGN
+                  </dd>
+                  <dd className="outline outline-red-500 block w-1/3 mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    support@credequity.com
+                  </dd>
+                </div>
+
+                <div className="bg-white flex flex-row justify-between px-4 py-5">
+                  {/* <dt className="text-sm font-medium text-gray-500">
+                    Application for
+                  </dt> */}
+                  <dd className="outline outline-red-500 block w-28 mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    Test Token
+                  </dd>
+                  <dd className="outline outline-red-500 block w-1/3 mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    Live Token
+                  </dd>
+                </div>
+                <div className="flex flex-row justify-between bg-gray-50 px-4 py-5">
+                  {/* <dt className="text-sm font-medium text-gray-500">
+                    Full name
+                  </dt> */}
+                  <dd className="outline outline-red-500 inline-block mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    csx982Ief5saa34gd
+                  </dd>
+                  <dd className="outline outline-red-500 block w-1/3 mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    009csx982IefFGop5saa34324gd
+                  </dd>
+                </div>
+
+                <div className="bg-white flex flex-row justify-between px-4 py-5">
+                  {/* <dt className="text-sm font-medium text-gray-500">
+                    Application for
+                  </dt> */}
+                  <dd className="outline outline-red-500 block w-28 mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    Documentation
+                  </dd>
+                </div>
+                <div className="bg-gray-50 px-4 py-5 ">
+                  <dd className="mt-1 text-sm text-gray-900">
                     Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim
                     incididunt cillum culpa consequat. Excepteur qui ipsum
                     aliquip consequat sint. Sit id mollit nulla mollit nostrud
@@ -139,81 +213,25 @@ export const ViewInstitution = () => {
                     adipisicing reprehenderit deserunt qui eu.
                   </dd>
                 </div>
-                <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <dt className="text-sm font-medium text-gray-500">
-                    Attachments
-                  </dt>
-                  <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                    <ul className="border border-gray-200 rounded-md divide-y divide-gray-200">
-                      <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-                        <div className="w-0 flex-1 flex items-center">
-                          {/* <!-- Heroicon name: solid/paper-clip --> */}
-                          <svg
-                            className="flex-shrink-0 h-5 w-5 text-gray-400"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            aria-hidden="true"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z"
-                              clipRule="evenodd"
-                            />
-                          </svg>
-                          <span className="ml-2 flex-1 w-0 truncate">
-                            {' '}
-                            resume_back_end_developer.pdf
-                            {' '}
-                          </span>
-                        </div>
-                        <div className="ml-4 flex-shrink-0">
-                          <a
-                            href="./"
-                            className="font-medium text-indigo-600 hover:text-indigo-500"
-                          >
-                            {' '}
-                            Download
-                            {' '}
-                          </a>
-                        </div>
-                      </li>
-                      <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-                        <div className="w-0 flex-1 flex items-center">
-                          {/* <!-- Heroicon name: solid/paper-clip --> */}
-                          <svg
-                            className="flex-shrink-0 h-5 w-5 text-gray-400"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            aria-hidden="true"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z"
-                              clipRule="evenodd"
-                            />
-                          </svg>
-                          <span className="ml-2 flex-1 w-0 truncate">
-                            {' '}
-                            coverletter_back_end_developer.pdf
-                            {' '}
-                          </span>
-                        </div>
-                        <div className="ml-4 flex-shrink-0">
-                          <a
-                            href="./"
-                            className="font-medium text-indigo-600 hover:text-indigo-500"
-                          >
-                            {' '}
-                            Download
-                            {' '}
-                          </a>
-                        </div>
-                      </li>
-                    </ul>
+
+                <div className="bg-white flex flex-row justify-between px-4 py-5">
+                  {/* <dt className="text-sm font-medium text-gray-500">
+                    Application for
+                  </dt> */}
+                  <dd className="outline outline-red-500 block w-28 mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    Description
                   </dd>
                 </div>
+                <div className="bg-gray-50 px-4 py-5 ">
+                  <dd className="mt-1 text-sm text-gray-900">
+                    Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim
+                    incididunt cillum culpa consequat. Excepteur qui ipsum
+                    aliquip consequat sint. Sit id mollit nulla mollit nostrud
+                    in ea officia proident. Irure nostrud pariatur mollit ad
+                    adipisicing reprehenderit deserunt qui eu.
+                  </dd>
+                </div>
+
               </dl>
             </div>
           </div>
