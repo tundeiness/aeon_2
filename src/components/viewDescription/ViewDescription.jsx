44aaa8f9afc2ba0460a739f04a7d80bfc111ad7a
tundeiness <tundeiness@yahoo.co.uk>
2022-06-16 18:58:58 +0100

    wip veiw description component

---
+++ src/components/viewDescription/ViewDescription.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const ViewDescription = ({
+  headingRight, contentRight, headingLeft, contentLeft,
+}) => {
+  const test = 0;
+  return (
+    <>
+      <div className="bg-white flex flex-row justify-between px-4 py-5">
+        <dd className="outline outline-red-500 block w-28 mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+          {headingLeft}
+        </dd>
+        <dd className="outline outline-red-500 block w-1/3 mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+          { headingRight}
+        </dd>
+      </div>
+      <div className="flex flex-row justify-between bg-gray-50 px-4 py-5">
+        <dd className="outline outline-red-500 block w-28 mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+          {contentLeft}
+        </dd>
+        <dd className="outline outline-red-500 block w-1/3 mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+          <button
+            className="bg-green-400 px-3 py-1 rounded-md text-white"
+            type="button"
+          >
+            {contentRight}
+          </button>
+        </dd>
+      </div>
+    </>
+  );
+};
+
+export default ViewDescription;
--- src/pages/institutions/view-institution/view-institution.jsx
+++ src/pages/institutions/view-institution/view-institution.jsx
@@ -52,10 +52,10 @@ export const ViewInstitution = () => {
                     Application for
                   </dt> */}
                   <dd className="outline outline-red-500 block w-28 mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                    CREDEQUITY
+                    Code
                   </dd>
                   <dd className="outline outline-red-500 block w-1/3 mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                    RC Number: 147749
+                    Status
                   </dd>
                 </div>
                 <div className="flex flex-row justify-between bg-gray-50 px-4 py-5">
