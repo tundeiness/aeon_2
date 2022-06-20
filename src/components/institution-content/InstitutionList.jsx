c0556e6598731406fa7e0ded3757890c2cf44bfe
tundeiness <tundeiness@yahoo.co.uk>
2022-06-20 13:30:56 +0100

    add border to name list

--- src/components/institution-content/InstitutionList.jsx
+++ src/components/institution-content/InstitutionList.jsx
@@ -111,7 +111,7 @@ const InstitutionList = () => {
             </header>
 
             <hr className="mb-5" />
-            <div className="search-wrapper relative">
+            <div className="mb-5 search-wrapper relative outline outline-red-500">
               <FiSearch className="absolute z-10 top-2.5 left-2 text-xl" />
               <input
                 type="search"
@@ -119,62 +119,67 @@ const InstitutionList = () => {
                 className="relative py-2 pl-8 w-1/4 rounded-lg outline outline-gray-300"
               />
             </div>
-            <div className="name-list">
-              <table className="table-fixed w-full text-xs">
-                <thead className=" bg-gray-50 text-xs capitalize">
-                  <tr>
-                    <th
-                      scope="col"
-                      className="w-12 text-gray-500 py-4 px-2 text-left"
-                    >
-                      S/N
-                    </th>
-                    <th
-                      scope="col"
-                      className="w-52 text-gray-500 py-4 text-center"
-                    >
-                      Name
-                    </th>
-                    <th
-                      scope="col"
-                      className=" flex items-center  text-gray-500 py-4 pl-20"
-                    >
-                      Status
-                      {/* <span>{ArrowDownIcon.symbol}</span> */}
-                      <BsArrowDownShort />
-                    </th>
-                    <th
-                      scope="col"
-                      className=" text-gray-500 py-4 pl-4 text-left"
-                    >
-                      Website
-                    </th>
-                    <th
-                      scope="col"
-                      className=" text-gray-500  py-4 pl-10 text-left"
-                    >
-                      Teams
-                    </th>
-                    <th scope="col" className=" text-gray-500 py-4 text-left" />
-                  </tr>
-                </thead>
-                <tbody className="divide-y divide-gray-300">
-                  {displayData}
-                </tbody>
-              </table>
+            <div className="border border-gray-200 rounded-lg">
+              <div className="name-list">
+                <table className="table-fixed w-full text-xs">
+                  <thead className=" bg-gray-50 text-xs capitalize">
+                    <tr>
+                      <th
+                        scope="col"
+                        className="w-12 text-gray-500 py-4 px-2 text-left"
+                      >
+                        S/N
+                      </th>
+                      <th
+                        scope="col"
+                        className="w-52 text-gray-500 py-4 text-center"
+                      >
+                        Name
+                      </th>
+                      <th
+                        scope="col"
+                        className=" flex items-center  text-gray-500 py-4 pl-20"
+                      >
+                        Status
+                        {/* <span>{ArrowDownIcon.symbol}</span> */}
+                        <BsArrowDownShort />
+                      </th>
+                      <th
+                        scope="col"
+                        className=" text-gray-500 py-4 pl-4 text-left"
+                      >
+                        Website
+                      </th>
+                      <th
+                        scope="col"
+                        className=" text-gray-500  py-4 pl-10 text-left"
+                      >
+                        Teams
+                      </th>
+                      <th
+                        scope="col"
+                        className=" text-gray-500 py-4 text-left"
+                      />
+                    </tr>
+                  </thead>
+                  <tbody className="divide-y divide-gray-300">
+                    {displayData}
+                  </tbody>
+                </table>
+              </div>
+              <ReactPaginate
+                previousLabel="Previous"
+                nextLabel="Next"
+                pageCount={pagingCount}
+                onPageChange={changePage}
+                containerClassName="pagination-button"
+                previousLinkClassName="previousButton"
+                nextLinkClassName="nextButton"
+                disabledClassName="paginationDisabled"
+                activeClassName="paginationActive"
+                className="w-full flex flex-row justify-around py-3 text-xs shadow-md"
+              />
             </div>
-            <ReactPaginate
-              previousLabel="Previous"
-              nextLabel="Next"
-              pageCount={pagingCount}
-              onPageChange={changePage}
-              containerClassName="pagination-button"
-              previousLinkClassName="previousButton"
-              nextLinkClassName="nextButton"
-              disabledClassName="paginationDisabled"
-              activeClassName="paginationActive"
-              className="w-full flex flex-row justify-around py-3 text-xs shadow-md"
-            />
           </div>
         </section>
       </article>
