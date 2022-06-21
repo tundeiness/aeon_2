d58812c48bdcef8ee88ac3c6d3e55e0f7ca4e348
tundeiness <tundeiness@yahoo.co.uk>
2022-06-21 18:00:15 +0100

    create services

---
+++ src/services/dashboardAPI.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const DashboardAPI = () => {
+  const test = 1;
+  return (
+    <div>DashboardAPI</div>
+  );
+};
+
+export default DashboardAPI;
