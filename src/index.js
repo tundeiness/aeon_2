046c4e56c5292ffbee81bed10210bf76a8ebb939
tundeiness <tundeiness@yahoo.co.uk>
2022-06-21 18:06:14 +0100

    import provider

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { ContextProvider } from './contexts/ContextProvider';
 import App from './App';
+import store from './app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter forceRefresh>
     <React.StrictMode>
       <ContextProvider>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </ContextProvider>
     </React.StrictMode>
   </BrowserRouter>,
