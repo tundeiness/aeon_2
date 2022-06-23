0350ac86621aa0769a6486f78b237fcd06c8bd3a
tundeiness <tundeiness@yahoo.co.uk>
2022-06-23 13:50:20 +0100

    edit store

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,13 +4,14 @@ import InstitutionReducer from './features/institutionSlice';
 import UserReducer from './features/userSlice';
 import LoginReducer from './features/loginSlice';
 import ProductReducer from './features/productSlice';
+import RoleReducer from './features/roleSlice';
 
 export default configureStore({
-  // app: DashboardReducer,
   reducer: {
     app: InstitutionReducer,
     user: UserReducer,
     login: LoginReducer,
     product: ProductReducer,
+    role: RoleReducer,
   },
 });
