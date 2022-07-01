d5727ef935f89cbf05541f01034234177dc0bb93
tundeiness <tundeiness@yahoo.co.uk>
2022-07-01 17:48:27 +0100

    clean up

--- src/pages/users/User.jsx
+++ src/pages/users/User.jsx
@@ -4,19 +4,7 @@
 /* eslint-disable no-unused-vars */
 import './users.css';
 import React, { useState } from 'react';
-// import { FiSearch, FiEdit2 } from 'react-icons/fi';
-// import { BsArrowDownShort } from 'react-icons/bs';
-// import { GoPrimitiveDot } from 'react-icons/go';
-// import { RiDeleteBinLine } from 'react-icons/ri';
-// import { Routes, Link, Outlet } from 'react-router-dom';
-// import ReactPaginate from 'react-paginate';
-// import SupportButton from '../../components/support/support';
-// import { useStateContext } from '../../contexts/ContextProvider';
-// import SidebarNav from '../../components/sideBarNav/sidebar-nav';
-// import DeleteInstitution from './deleteInstitution/DeleteInstitution';
-// import InstitutionList from '../../../components/institutionContent/InstitutionList';
 import UserList from '../../components/user-content/UserList';
-// import Data from '../../data/MOCK_DATA.json';
 
 const User = () => {
   const test = 0;
