7800cb06eb92eb890564a99ba127acecf1155281
tundeiness <tundeiness@yahoo.co.uk>
2022-07-05 14:03:01 +0100

    create accounts page

---
+++ src/pages/accounts/daily-utilization.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const DailyUtilization = () => {
+  const test = 0;
+  return (
+    <div>daily-utilization</div>
+  );
+};
+
+export default DailyUtilization;
