be60520927a9e786850a1f7d940870ba8243b084
tundeiness <tundeiness@yahoo.co.uk>
2022-07-08 09:36:16 +0100

    edit role slice

--- src/redux/features/roleSlice.js
+++ src/redux/features/roleSlice.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable no-param-reassign */
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
@@ -26,20 +27,6 @@ const initialState = {
 export const roleSlice = createSlice({
   name: 'role',
   initialState,
-
-  // : {
-  //   roleInfo: {
-  //     description: 'This Role is a User Role',
-  //     status: 'Active',
-  //     code: '004',
-  //     id: '7179b718-4c94-49bf-a6fe-1eb2cdc82530',
-  //     name: 'User',
-  //     normalizedName: 'USER',
-  //     concurrencyStamp: 'f165e0b8-440e-446b-a5b4-d28a70cb5361',
-  //   },
-  //   pending: false,
-  //   error: false,
-  // },
   reducers: {},
   extraReducers(builder) {
     builder
@@ -50,7 +37,7 @@ export const roleSlice = createSlice({
         state.status = 'succeeded';
         state.role = action.payload;
       })
-      .addCase(getAllRoles.fulfilled, (state, action) => {
+      .addCase(getAllRoles.rejected, (state, action) => {
         state.status = 'failed';
         state.error = action.error.message;
       });
