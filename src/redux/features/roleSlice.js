16ed3555fe4d690ccff0173cd9cd165f2d7e619b
tundeiness <tundeiness@yahoo.co.uk>
2022-07-08 09:29:10 +0100

    complete role slice

--- src/redux/features/roleSlice.js
+++ src/redux/features/roleSlice.js
@@ -17,36 +17,48 @@ export const getAllRoles = createAsyncThunk(
   },
 );
 
+const initialState = {
+  role: [],
+  status: 'idle',
+  error: null,
+};
+
 export const roleSlice = createSlice({
   name: 'role',
-  initialState: {
-    roleInfo: {
-      description: 'This Role is a User Role',
-      status: 'Active',
-      code: '004',
-      id: '7179b718-4c94-49bf-a6fe-1eb2cdc82530',
-      name: 'User',
-      normalizedName: 'USER',
-      concurrencyStamp: 'f165e0b8-440e-446b-a5b4-d28a70cb5361',
-    },
-    pending: false,
-    error: false,
-  },
+  initialState,
+
+  // : {
+  //   roleInfo: {
+  //     description: 'This Role is a User Role',
+  //     status: 'Active',
+  //     code: '004',
+  //     id: '7179b718-4c94-49bf-a6fe-1eb2cdc82530',
+  //     name: 'User',
+  //     normalizedName: 'USER',
+  //     concurrencyStamp: 'f165e0b8-440e-446b-a5b4-d28a70cb5361',
+  //   },
+  //   pending: false,
+  //   error: false,
+  // },
   reducers: {},
-  extraReducers: {
-    [getAllRoles.pending]: (state) => {
-      state.pending = true;
-      state.error = false;
-    },
-    [getAllRoles.fulfilled]: (state, action) => {
-      state.pending = true;
-      state.userInfo = action.payload;
-    },
-    [getAllRoles.rejected]: (state) => {
-      state.pending = false;
-      state.error = true;
-    },
+  extraReducers(builder) {
+    builder
+      .addCase(getAllRoles.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(getAllRoles.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.role = action.payload;
+      })
+      .addCase(getAllRoles.fulfilled, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
   },
 });
+
+export const selectAllRoles = (state) => state.role.role;
+export const getRoleStatus = (state) => state.role.status;
+export const getRoleError = (state) => state.role.error;
 
 export default roleSlice.reducer;
