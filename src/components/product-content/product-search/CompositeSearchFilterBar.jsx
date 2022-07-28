c92f515fa274313fbe3a7d15434b041d211e051d
tundeiness <tundeiness@yahoo.co.uk>
2022-07-28 20:46:59 +0100

    remove excess code blocks

--- src/components/product-content/product-search/CompositeSearchFilterBar.jsx
+++ src/components/product-content/product-search/CompositeSearchFilterBar.jsx
@@ -2,9 +2,7 @@
 /* eslint-disable no-unused-vars */
 import React, { useRef } from 'react';
 import { useDispatch } from 'react-redux';
-import { FiSearch } from 'react-icons/fi';
-import { GoButton, SearchButton } from '../../Buttons/buttonCollections';
-import { searchedInstitution } from '../../../redux/features/institutionSlice';
+import { SearchButton } from '../../Buttons/buttonCollections';
 import {
   searchProductByCode,
   searchProductByName,
@@ -13,13 +11,12 @@ import ProductFilterBar from '../product-filter/ProductFilterBar';
 
 const CompositeSearchFilterBar = () => {
   const nameRef = useRef('');
-  const searchRef = useRef('');
   const codeRef = useRef('');
   const dispatch = useDispatch();
 
   const handleSearchProduct = () => {
-    if (searchRef.current.value) {
-      dispatch(searchProductByCode(searchRef.current.value));
+    if (codeRef.current.value) {
+      dispatch(searchProductByCode(codeRef.current.value));
     }
 
     if (nameRef.current.value) {
@@ -27,24 +24,6 @@ const CompositeSearchFilterBar = () => {
     }
   };
   return (
-  // <div className="flex flex-row justify-end w-1/2 py-2">
-  //   <div className="flex flex-row  items-center space-x-4 w-4/5">
-  //     <div className="search-wrapper w-full relative">
-  //       <FiSearch className="absolute z-10 top-3.5 left-2 text-xl" />
-  //       <input
-  //         type="search"
-  //         placeholder="Search"
-  //         className="relative pl-8 py-3 rounded-lg w-full border border-gray-200 focus:outline-none"
-  //         onChange={handleSearchInstitution}
-  //         ref={searchRef}
-  //       />
-  //     </div>
-  //     <div className="flex flex-col items-center justify-center">
-  //       <GoButton />
-  //     </div>
-  //   </div>
-  // </div>
-
     <div className="flex flex-row justify-between w-full mb-5">
       <div className="w-1/4 mr-2">
         <label
@@ -77,7 +56,7 @@ const CompositeSearchFilterBar = () => {
           type="text"
           name="code"
           id="code"
-          ref={searchRef}
+          ref={codeRef}
           // onChange={handleSearchProduct}
         />
       </div>
