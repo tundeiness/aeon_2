c56b3ecb7bf3418e84a7785f284b105f23307e1c
tundeiness <tundeiness@yahoo.co.uk>
2022-07-29 09:57:28 +0100

    create search and filter bars for user page

---
+++ src/components/user-content/NameStatusRoleFilterBar/NameStatusRoleFilterBar.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const NameStatusRoleFilterBar = () => {
+  const test = 0;
+  return (
+    <div>NameStatusRoleFilterBar</div>
+  );
+};
+
+export default NameStatusRoleFilterBar;
---
+++ src/components/user-content/SearchUserBar/SearchUserBar.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const SearchUserBar = () => {
+  const test = 0;
+  return (
+    <div>SearchUserBar</div>
+  );
+};
+
+export default SearchUserBar;
